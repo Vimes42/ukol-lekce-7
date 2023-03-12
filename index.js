9658b890308540ac9eabdc7a83720dba661fde6a
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-12 17:02:00 +0100

    úkol č. 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// úkol č.1
+
 const salary = (wage, hours, days) => {
     return wage * hours * days
 }
@@ -21,4 +23,31 @@ document.body.innerHTML = `
     <p> Tax: ${tax} % </p>
     <p> Gross wage before taxation: ${grossWage} </p>
     <p> Taxed wage: ${taxedWage} </p>
+`
+
+// úkol č.2
+const calculate = (number1, operation, number2) => {
+    if (operation === "+") {
+        return number1 + number2
+    } else if (operation === "-") {
+        return number1 - number2
+    } else if (operation === "*" ) {
+        return number1 * number2
+    } else if (operation ==="/") {
+        return number1 / number2
+    } else {
+        return "wrong input"
+    }
+}
+
+const addition = calculate(2, "+", 3)
+const subtraction = calculate(2, "-", 3)
+const multiplication = calculate(2, "*", 3)
+const division = calculate(2, "/", 3)
+
+document.body.innerHTML += `
+    <p> 2 + 3 = ${addition} </p>
+    <p> 2 - 3 = ${subtraction} </p>
+    <p> 2 * 3 = ${multiplication} </p>
+    <p> 2 / 3 = ${division} </p>
 `
